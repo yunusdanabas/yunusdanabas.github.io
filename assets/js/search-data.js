a3856a55a119580b565f08c3ab48b11a41777965
yunusdanabas <76391173+yunusdanabas@users.noreply.github.com>
2025-02-14 18:21:59 +0000

    Deploying to gh-pages from @ yunusdanabas/yunusdanabas.github.io@52f08d1b1adfb4c28a1230ebb3b8ec50da9fe0be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
-          description: "",
+  },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/research/";
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
           },
         },{id: "post-a-post-with-image-galleries",
       
