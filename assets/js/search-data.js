ae9edf2c2214e5750397e08a9398d14f8971dfe7
yunusdanabas <76391173+yunusdanabas@users.noreply.github.com>
2025-03-30 14:39:47 +0000

    Deploying to gh-pages from @ yunusdanabas/yunusdanabas.github.io@3009cbf32a1c044980f8b2a0b31c2969b4b072b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A concise summary of my education, work experience, projects, and skills in robotics, mechatronics, and automation.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -425,14 +425,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%75%6E%75%73%64%61%6E%61%62%61%73@%73%61%62%61%6E%63%69%75%6E%69%76.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/yunusdanabas", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/yunus-emre-danabas", "_blank");
         },
       },{
         id: 'social-rss',
@@ -440,20 +447,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
