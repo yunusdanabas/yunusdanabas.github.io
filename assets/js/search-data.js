eed203e58e111120d1a7ad85aaf2a1274b3cd453
yunusdanabas <76391173+yunusdanabas@users.noreply.github.com>
2025-04-13 13:01:45 +0000

    Deploying to gh-pages from @ yunusdanabas/yunusdanabas.github.io@f1818848a35d3607a24d88bbfa007bea184c3862 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -385,14 +385,14 @@ ninja.data = [{
           description: "An advanced ROS project demonstrating autonomous frontier exploration, SLAM, and multi-robot map merging with TurtleBot3 in a simulated Gazebo environment.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-how-can-a-robot-place-an-item-on-a-cluttered-desk",
+          title: 'How can a robot place an item on a cluttered desk?',
+          description: "PAGE WİLL BE UPDATED",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-panda-base-simulator",
+          title: 'Panda Base Simulator',
+          description: "IT WILL BE UPDATED",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-2",
