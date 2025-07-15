b4ef5eebe48b2b35abd9e883df454c6800d7ef2e
yunusdanabas <76391173+yunusdanabas@users.noreply.github.com>
2025-07-15 15:37:12 +0000

    Deploying to gh-pages from @ yunusdanabas/yunusdanabas.github.io@452c3a45c99b80c73d31fd7e20314d5df87e4231 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A curated collection of my projects—from extracurricular endeavors to course-based work. Click on a project for full information.",
+          description: "A curated collection of my work—from extracurricular initiatives to course-based assignments. Click on a project for full details.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
